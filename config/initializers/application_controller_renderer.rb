ddb81246f8947e93a265681efe53dac16bb323bd
mapi8808 <mapi.008.k@gmail.com>
2020-11-20 09:23:57 +0000

    [add]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a91e82f9ed91401df035fb58c1ac41e58029f4a3b69273f9bf37d3bdf14c4caaa46485a3d7ab3e7b2c3df750284e7e4a0b4114848e9cb191712457f8981c47fb'
